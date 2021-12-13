35ed3b0dd8ca95a0e20f4513a96c91c02a983092
Mazen Kapadi <73120392+mazenkapadi@users.noreply.github.com>
2021-12-13 15:29:16 +0530

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Mazen Kapadi on 13/12/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
